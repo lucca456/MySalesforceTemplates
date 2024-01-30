6eb31669361e19b3d403255a6f8b6e194e2d40ae
lucca guarreschi <luccaguareschi@gmail.com>
2024-01-29 23:10:44 -0300

    lwc client pagination template

---
+++ force-app/main/default/lwc/mmsClientPaginationTemplate/mmsClientPaginationTemplate.js
@@ -0,0 +1,25 @@
+import { LightningElement, wire } from 'lwc';
+import getRecordList from '@salesforce/apex/MMSTemplaPagination.getRecordList'
+
+export default class MmsClientPaginationTemplate extends LightningElement {
+	totalRecords;
+	visibleRecords;
+
+	@wire(getRecordList)
+	wiredRecord({error, data}){
+		if(data){
+			this.totalRecords = data;
+			console.log(this.totalRecords);
+		}
+		if(error){
+			console.error(error);
+		}
+	}
+
+	updateRecordHandler(event){
+		this.visibleRecords = [... event.detail.records];
+		console.log(event.detail.records);
+	}
+	
+
+}
