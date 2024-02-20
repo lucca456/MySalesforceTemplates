a8877aed58183d188d1dfbcf0f4044b724f3c56e
lucca guarreschi <luccaguareschi@gmail.com>
2024-02-20 00:54:39 -0300

    Added methods to component

--- force-app/main/default/lwc/mmsViewCallouts/mmsViewCallouts.js
+++ force-app/main/default/lwc/mmsViewCallouts/mmsViewCallouts.js
@@ -1,5 +1,45 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
+import makeGetCallout from '@salesforce/apex/MMSTemplateCallouts.makeGetCallout';
+import makePostCallout from '@salesforce/apex/MMSTemplateCallouts.makePostCallout';
+import makePatchCallout from '@salesforce/apex/MMSTemplateCallouts.makePatchCallout';
+import makePutCallout from '@salesforce/apex/MMSTemplateCallouts.makePutCallout';
+import makeDeleteCallout from '@salesforce/apex/MMSTemplateCallouts.makeDeleteCallout';
 export default class MmsViewCallouts extends LightningElement {
 
+	@track 
+	requestBody;
+    @track 
+	responseBody;
+
+
+	async handleGetCallout(){
+        const result = await makeGetCallout({});
+        this.requestBody = result.requestBody;
+        this.responseBody = result.responseBody;
+    }
+
+	async handlePostCallout(){
+        const result = await makePostCallout({});
+        this.requestBody = result.requestBody;
+        this.responseBody = result.responseBody;
+    }
+
+	async handlePatchCallout(){
+        const result = await makePatchCallout({});
+        this.requestBody = result.requestBody;
+        this.responseBody = result.responseBody;
+    }
+
+	async handlePutCallout(){
+        const result = await makePutCallout({});
+        this.requestBody = result.requestBody;
+        this.responseBody = result.responseBody;
+    }
+
+	async handleDeleteCallout(){
+        const result = await makeDeleteCallout({});
+        this.requestBody = result.requestBody;
+        this.responseBody = result.responseBody;
+    }
 }
